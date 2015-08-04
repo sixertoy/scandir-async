b9579f5db394a11e10ecb8f63f6923ef81ee99ff
sixertoy <sixertoy.github@gmail.com>
2015-08-04 19:00:44 +0200

    fix sort lowercase

--- src/lib/scandir-async.js
+++ src/lib/scandir-async.js
@@ -5,9 +5,9 @@
     'use strict';
 
     function sortByName(a, b) {
-        if (a.name < b.name)
+        if (a.name.toLowerCase() < b.name.toLowerCase())
             return -1;
-        if (a.name > b.name)
+        if (a.name.toLowerCase() > b.name.toLowerCase())
             return 1;
         return 0;
     }
